466de3c7dff67b0ea41849f7e33a7b4b9fdaae91
snajera531 <salemnajera@gmail.com>
2021-06-02 16:03:54 -0600

    im smart for once

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Animator animator;
     public Health health;
     public float Speed { get; set; } = 10.0f;
     public float Strength { get; set; } = 1.0f;
+    public Transform attackPoint;
+    private float attackReach = 2.0f;
+    private Vector3 playerDirection;
 
     void Start()
     {
@@ -15,20 +19,30 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        Vector3 direction = Vector3.zero;
+        playerDirection = Vector3.zero;
 
-        direction.x = Input.GetAxis("Horizontal");
-        direction.y = Input.GetAxis("Vertical");
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Attack();
+        }
 
-        Vector3 velocity = direction * Speed;
+        playerDirection.x = Input.GetAxis("Horizontal");
+        playerDirection.y = Input.GetAxis("Vertical");
+
+        Vector3 velocity = playerDirection * Speed;
         transform.position += velocity * Time.deltaTime;
     }
 
     public void Attack()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        //play attack animation
+        Collider2D[] collidingEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackReach);
+        foreach(Collider2D enemy in collidingEnemies)
         {
-            
+            if(enemy.TryGetComponent(out Health h))
+            {
+                h.TakeDamage(Strength);
+            }
         }
     }
 
