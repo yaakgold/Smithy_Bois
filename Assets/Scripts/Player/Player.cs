664db6e97a8f58ffd37568ea53f2aa7c6ad06d5f
snajera531 <salemnajera@gmail.com>
2021-06-02 15:11:32 -0600

    yEET

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -18,7 +18,7 @@ public class Player : MonoBehaviour
         Vector3 direction = Vector3.zero;
 
         direction.x = Input.GetAxis("Horizontal");
-        direction.z = Input.GetAxis("Vertical");
+        direction.y = Input.GetAxis("Vertical");
 
         Vector3 velocity = direction * Speed;
         transform.position += velocity * Time.deltaTime;
