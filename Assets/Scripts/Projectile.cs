b0ebe84b63a4df304d86d3a233e0474e3d011ac6
yaakgold <yaakovgoldberg007@gmail.com>
2021-06-03 19:39:09 -0400

    Projectile setup (do not touch yet)

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float damage;
+    public string tagHit, tagIgnore;
+    public float speed;
+
+    public void Fire(float _speed, Transform target, float _damage, string tagOfObjectToHit, string tagOfObjectToIgnore)
+    {
+        damage = _damage;
+        tagHit = tagOfObjectToHit;
+        tagIgnore = tagOfObjectToIgnore;
+        speed = _speed;
+
+        GetComponent<Rigidbody2D>().AddForce((target.position - transform.position).normalized * speed, ForceMode2D.Impulse);
+    }
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.forward, speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag(tagHit))
+        {
+            if(collision.gameObject.TryGetComponent(out Health h))
+            {
+                h.TakeDamage(damage);
+            }
+        }
+        
+        if(!collision.gameObject.CompareTag(tagIgnore))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
